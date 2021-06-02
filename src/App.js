1141fc05216cdba9c7483b0d0a2f8e828ad300c1
malathi <malathi.balla@kore.com>
2021-06-02 12:48:20 +0530

    Products filtering functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import React from "react";
 import data from "./data.json";
 import Products from "./components/Products";
+import Filter from "./components/Filter";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      prodcuts: data.products,
+      products: data.products,
       size: "",
       sort: "",
     };
   }
+
+  handleOrder = (e) => {
+    console.log("-----------", e.target.value);
+    const sortVal = e.target.value;
+    this.setState({
+      sort: sortVal,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sortVal === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sortVal === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        ),
+    });
+  };
+
+  handleFilter = (e) => {
+    console.log("-----------", e.target.value);
+    if (e.target.value === "") {
+      this.setState({
+        size: e.target.value,
+        products: data.products,
+      });
+    } else {
+      console.log("in else");
+      this.setState({
+        size: e.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(e.target.value) >= 0
+        ),
+      });
+    }
+  };
   render() {
     return (
       <div className="grid-container">
@@ -18,9 +60,16 @@ class App extends React.Component {
           <a href="/">Shopping Cart</a>
         </header>
         <main>
+          <Filter
+            items={this.state.products.length}
+            sort={this.state.sort}
+            size={this.state.size}
+            handleOrder={this.handleOrder}
+            handleFilter={this.handleFilter}
+          ></Filter>
           <div className="content">
             <div className="main">
-              <Products products={this.state.prodcuts}></Products>
+              <Products products={this.state.products}></Products>
             </div>
             <div className="sidebar">Cart Items</div>
           </div>
